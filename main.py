2bfbe787296cad98efcd8fc4a38fd6bc51ba6df8
cx2889 <523764720@qq.com>
2019-06-21 00:34:53 +0800

    网页的开关接口已经做好了

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import wifi_handler
-import charging_handler
 import web_handler
 
 
 wifi_handler.connect()
 web_handler.start()
 
-print("===== system ready =====")
+print("\n===== system ready =====\n")
--- web_handler.py
+++ web_handler.py
@@ -1,4 +1,5 @@
 from microWebSrv import MicroWebSrv
+import charging_handler
 
 mws = None
 
@@ -6,3 +7,21 @@ def start():
     global mws
     mws = MicroWebSrv(webPath="/")
     mws.Start(threaded=True)
+
+def send_ok(httpResponse):
+    httpResponse.WriteResponseOk( headers         = None,
+                                contentType     = "text/html",
+                                contentCharset  = "UTF-8",
+                                content         = "ok" )
+
+@MicroWebSrv.route('/power_on')
+def handlerFuncGet(httpClient, httpResponse) :
+    print("power_on")
+    charging_handler.charging_state(1)
+    send_ok(httpResponse)
+
+@MicroWebSrv.route('/power_off')
+def handlerFuncGet(httpClient, httpResponse) :
+    print("power_off")
+    charging_handler.charging_state(0)
+    send_ok(httpResponse)
