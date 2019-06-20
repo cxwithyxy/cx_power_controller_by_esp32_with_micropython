32d1ee1783fe12dc2f586bc3261fff0663adffe1
cx2889 <523764720@qq.com>
2019-06-20 23:31:22 +0800

    充电和断开的函数搞好了

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from machine import Pin
 import utime
-p4 = Pin(4, Pin.OUT)
+
+def charging_state(key = None):
+    p4 = Pin(4, Pin.OUT)
+    if key is None:
+        return p4.value()
+    else:
+        p4.value(key)
+
+def switch_charging_state():
+    if charging_state() > 0:
+        charging_state(0)
+    else:
+        charging_state(1)
 
 while True:
     utime.sleep(3)
-    if p4.value() > 0:
-        p4.value(0)
-    else:
-        p4.value(1)
+    switch_charging_state()
 
